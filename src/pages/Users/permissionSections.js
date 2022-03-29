06d4d4b1cb39dad29d8d1a3c5f7de04db6309107
Mateo Hoxha <mateohoxha26@gmail.com>
2022-03-29 20:17:53 +0200

    small change

--- src/pages/Users/permissionSections.js
+++
@@ -1,8 +0,0 @@
-export const permissionSection = {
-  Produktet: ['Shiko', 'Krijo', 'Perditeso', 'Fshi'],
-  Kategorite: {},
-  'Njesite e shitjes': {},
-  'Pikat e shitjes': {},
-  Furnizuesit: {},
-  Perdoruesit: {},
-};
