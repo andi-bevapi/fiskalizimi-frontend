2de9fe954bd80af833020d32b6f4f1430e7ac968
Endi Tabaku <endi.tabaku@hotmail.com>
2022-03-31 23:26:41 +0200

    Enable Barcode scanning per product and checks if barcode exists

--- src/components/Modal/SwalModal.js
+++ src/components/Modal/SwalModal.js
@@ -11,14 +11,14 @@ export const SwalModal = (
   identifier
 ) => {
   Swal.fire({
-    title,
+    title : "<h5 style='font-family: Poppins; font-size: 20px; color: #082e2b; font-weight: 600'>" + title + "</h5>",
     text,
     icon,
     showCancelButton: true,
     showConfirmButton: confirmButtonText === "" ? false : true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    cancelButtonText,
+    cancelButtonText: "<span style='font-family: Poppins;'>" + cancelButtonText + "</span>",
     confirmButtonText,
   }).then((result) => {
     if (result.isConfirmed) {
