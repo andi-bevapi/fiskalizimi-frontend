b8df775807be3314d411a92ddda96a05afddf6d1
marselina_d <dollapajmarselinda@gmail.com>
2022-04-14 16:13:58 +0200

    get input from scanner

--- src/layouts/Body/OnBuy/SearchByBarcode/index.js
+++ src/layouts/Body/OnBuy/SearchByBarcode/index.js
@@ -2,13 +2,14 @@ import { makeStyles } from '@mui/styles';
 import IconButtonComponent from '../../../../components/Button/IconButton';
 import BootstrapInputField from '../../../../components/InputFields/BootstrapTextField';
 import Grid from '@mui/material/Grid';
-import { React, useState, useCallback, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-import BottomContainer from './../../../../components/BottomContainer';
-import Drawer from "react-bottom-drawer";
+// import BottomContainer from './../../../../components/BottomContainer';
+// import Drawer from "react-bottom-drawer";
 import { useInvoiceContext } from '../../../../Context/InvoiceContext';
 import { useContextProduct } from '../../../../Context/ProductContext';
 import { SwalModal } from '../../../../components/Modal/SwalModal';
+import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
 
 const useStyles = makeStyles(() => ({
   searchInput: { display: 'block' },
@@ -21,66 +22,80 @@ const SearchByBarcode = (props) => {
   const { listedInvoiceProducts, addToInvoiceList } = useInvoiceContext();
   const { getProductByBarcode } = useContextProduct();
   const [barcode, setBarcode] = useState('');
-  const [isVisible, setIsVisible] = useState(false);
-  const [searchedProducts, setSearchedProducts] = useState({});
+  // const [isVisible, setIsVisible] = useState(false);
+  // const [searchedProducts, setSearchedProducts] = useState({});
 
-  const [scannedProduct, setScannedProduct] = useState({});
-  const [stopAddingProduct, setStopAddingProduct] = useState(false);
-  const [hasBarcodeData, setHasBarcodeData] = useState(false);
+  // const [scannedProduct, setScannedProduct] = useState({});
+  // const [stopAddingProduct, setStopAddingProduct] = useState(false);
+  // const [hasBarcodeData, setHasBarcodeData] = useState(false);
 
-  const openDrawer = useCallback(() => setIsVisible(true), []);
-  const closeDrawer = useCallback(() => setIsVisible(false), []);
+  // const openDrawer = useCallback(() => setIsVisible(true), []);
+  // const closeDrawer = useCallback(() => setIsVisible(false), []);
 
   useEffect(() => {
 
   }, [listedInvoiceProducts])
 
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      if (barcode.trim().length > 0) scanBarcode();
+    }, 100);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+
+  }, [barcode]);
+
   const handleChange = (event) => {
     setBarcode(event.target.value);
   };
 
-  const handleSearchField = () => {
-    (barcode.length == 0 ? null : (searchByBarcode()))
-  }
+  // const handleSearchField = () => {
+  //   (barcode.length == 0 ? null : (searchByBarcode()))
+  // }
 
-  const searchByBarcode = async () => {
-    const searchedProductBarcode = await props.searchFunction(barcode);
-    setSearchedProducts(searchedProductBarcode);
-    openDrawer();
-  }
+  // const searchByBarcode = async () => {
+  //   const searchedProductBarcode = await props.searchFunction(barcode);
+  //   setSearchedProducts(searchedProductBarcode);
+  //   openDrawer();
+  // }
 
-  const checkKeyPressed = (event) => {
-    (event.key === 'Enter' ? (scanBarcode()) : null);
-  }
+  // const checkKeyPressed = (event) => {
+  //   (event.key === 'Enter' ? (scanBarcode()) : null);
+  // }
 
   const scanBarcode = async () => {
-    const product = await getProductByBarcode(barcode);
+    const product = await getProductByBarcode(barcode.trim());
     if (product) {
-      if (Number(product.stock).toFixed(0) != 0) {
-        setScannedProduct(product)
-        setHasBarcodeData(true);
+      // if (Number(product.stock).toFixed(0) != 0) {
+        // setScannedProduct(product)
+        // setHasBarcodeData(true);
         const productFromInvoice = listedInvoiceProducts?.filter(item => item.id === product.id);
         if (productFromInvoice.length > 0) {
           if (productFromInvoice[0].stockCheck) {
-            if (productFromInvoice[0].quantity >= product.stock) {
-              setStopAddingProduct(true);
-            } else {
+            // if (productFromInvoice[0].quantity >= product.stock) {
+            //   setStopAddingProduct(true);
+            // } else {
               addToInvoiceList(product, productFromInvoice[0].quantity + 1);
-            }
+            // }
           } else {
             addToInvoiceList(product, productFromInvoice[0].quantity + 1);
           }
         } else {
           addToInvoiceList(product, 1)
         }
-      }else{
-        setHasBarcodeData(false);
-        handleSwalNoStock();
-      }
+      // }else{
+      //   setHasBarcodeData(false);
+      //   handleSwalNoStock();
+      // }
     } else {
-      setHasBarcodeData(false);
+      // setHasBarcodeData(false);
       handleSwal();
     }
+    setTimeout(() => {
+      setBarcode('');
+    }, 500);
   }
 
   const handleSwal = async () => {
@@ -116,19 +131,18 @@ const SearchByBarcode = (props) => {
             style={{ margin: 0 }}
             value={barcode}
             onChange={handleChange}
-            onKeyDown={checkKeyPressed}
+            autoFocus={true}
           />
         </Grid>
         <Grid item={true} xs={2} md={2}>
           <IconButtonComponent
-            style={{ backgroundColor: '#12AC7A', height: 40, width: 40 }}
-            icon={<ManageSearchIcon />}
+            style={{ backgroundColor: '#12AC7A', height: 40, width: 40, cursor: 'default'}}
+            icon={<CenterFocusStrongIcon />}
             iconColor={{ color: '#fff' }}
-            onClick={handleSearchField}
           />
         </Grid>
       </Grid>
-      <>
+      {/* <>
         <Drawer
           duration={250}
           hideScrollbars={false}
@@ -138,7 +152,7 @@ const SearchByBarcode = (props) => {
         >
           <BottomContainer searchedItem={searchedProducts} barcode={barcode} addToList={addToInvoiceList} invoiceList={listedInvoiceProducts} />
         </Drawer>
-      </>
+      </> */}
     </div>
   );
 };
