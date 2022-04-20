f00c6345f14a5de68c89641539e474435f2bc643
marselina_d <dollapajmarselinda@gmail.com>
2022-04-20 15:25:21 +0200

    Update i gjendjes se arkes

--- src/Context/MoneyDepositContext.js
+++ src/Context/MoneyDepositContext.js
@@ -1,27 +1,78 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { useModel } from 'umi';
+import { getLastAmount, updateSavedAmount } from '../services/arka';
 
 const MoneyDepositContext = createContext({});
 
 const MoneyDepositProvider = (props) => {
     const { initialState } = useModel('@@initialState');
     const [isLoading, setIsLoading] = useState(false);
-    const [ depositAmount , setDepositAmunt] = useState(5000);
+    const [ depositAmount , setDepositAmunt] = useState(0.00);
 
     useEffect(() => {
-
+        getActualAmount();
     }, []);
 
-    const updateAmount = (newAmount) => {
-        setDepositAmunt(newAmount);
+    const getActualAmount = async () => {
+        setIsLoading(true);
+        try {
+            ///! get arkaId !!!
+            const response = await getLastAmount(1);
+            if (response.statusCode === 200) {
+                setDepositAmunt(response.data.totalAmount);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        setIsLoading(false);
     }
 
-    const addAmountToDeposit = (value) => {
-        setDepositAmunt(depositAmount + value );
+    const updateAmount = async (newAmount) => {
+        try {
+            const data = {
+                totalAmount: newAmount,
+                arkaId: 1, // !!get arkaId  !!!
+                userId: initialState?.currentUser?.id,
+                action: "gjendje re",
+                actionTime: new Date()
+            }
+            const response = await updateSavedAmount(data);
+            if (response.statusCode === 200) setDepositAmunt(newAmount);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    const reduceAmountFromDeposit = (value) => {
-        setDepositAmunt(depositAmount - value );
+    const addAmountToDeposit = async (value) => {
+        try {
+            const data = {
+                totalAmount: depositAmount + value,
+                arkaId: 1, // !!get arkaId  !!!
+                userId: initialState?.currentUser?.id,
+                action: `+${value}`,
+                actionTime: new Date()
+            }
+            const response = await updateSavedAmount(data);
+            if (response.statusCode === 200) setDepositAmunt(depositAmount + value );
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const reduceAmountFromDeposit = async (value) => {
+        try {
+            const data = {
+                totalAmount: depositAmount - value,
+                arkaId: 1, // !!get arkaId  !!!
+                userId: initialState?.currentUser?.id,
+                action: `-${value}`,
+                actionTime: new Date()
+            }
+            const response = await updateSavedAmount(data);
+            if (response.statusCode === 200) setDepositAmunt(depositAmount - value );
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const values = {depositAmount, updateAmount, addAmountToDeposit, reduceAmountFromDeposit}
