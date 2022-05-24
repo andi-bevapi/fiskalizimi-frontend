727a1dfce0e1ac2ac2824085a4ad2987b14ef573
marselina_d <dollapajmarselinda@gmail.com>
2022-05-24 12:33:45 +0200

    added pagination in home page

--- src/pages/Dashboard/Body.js
+++ src/pages/Dashboard/Body.js
@@ -3,6 +3,10 @@ import ItemCard from './Item/ItemCard';
 import ItemLine from './Item/ItemLine';
 import NoData from '../../components/NoData';
 import Grid from '@mui/material/Grid';
+import ReactPaginate from 'react-paginate';
+import React, { useState } from 'react';
+import styles from './Body.module.css';
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(() => ({
   body: {
@@ -24,7 +28,17 @@ const useStyles = makeStyles(() => ({
 }));
 
 const BodyDashboard = (props) => {
+  const { t } = useTranslation();
   const classes = useStyles();
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 8;
+  const pagesVisited = pageNumber * itemsPerPage;
+  const pageCount = Math.ceil(props.data?.length / itemsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className={classes.body}>
       {props.data?.length > 0 ? (
@@ -32,17 +46,27 @@ const BodyDashboard = (props) => {
           {props.display === 'cards' ? (
             <div>
               <Grid container spacing={3}>
-                {props.data?.map((item, index) => {
+                {props.data?.slice(pagesVisited, pagesVisited + itemsPerPage).map((item, index) => {
                   if (props.data?.length === index + 1) {
                     return (
                       <Grid item xs={12} sm={4} md={3} lg={3} xl={2} key={index}>
-                        <ItemCard key={index} item={item} addToInvoiceList={props.addToInvoiceFunc} invoiceList={props.invoiceList} />
+                        <ItemCard
+                          key={index}
+                          item={item}
+                          addToInvoiceList={props.addToInvoiceFunc}
+                          invoiceList={props.invoiceList}
+                        />
                       </Grid>
                     );
                   } else {
                     return (
                       <Grid item xs={12} sm={4} md={3} lg={3} xl={2} key={index}>
-                        <ItemCard key={index} item={item} addToInvoiceList={props.addToInvoiceFunc} invoiceList={props.invoiceList} />
+                        <ItemCard
+                          key={index}
+                          item={item}
+                          addToInvoiceList={props.addToInvoiceFunc}
+                          invoiceList={props.invoiceList}
+                        />
                       </Grid>
                     );
                   }
@@ -50,14 +74,41 @@ const BodyDashboard = (props) => {
               </Grid>
             </div>
           ) : (
-            props.data?.map((item, index) => {
+            props.data?.slice(pagesVisited, pagesVisited + itemsPerPage).map((item, index) => {
               if (props.data.length === index + 1) {
-                return <div key={index}><ItemLine item={item} addToInvoiceList={props.addToInvoiceFunc} invoiceList={props.invoiceList} /></div>;
+                return (
+                  <div key={index}>
+                    <ItemLine
+                      item={item}
+                      addToInvoiceList={props.addToInvoiceFunc}
+                      invoiceList={props.invoiceList}
+                    />
+                  </div>
+                );
               } else {
-                return <div key={index}><ItemLine item={item} addToInvoiceList={props.addToInvoiceFunc} invoiceList={props.invoiceList} /></div>;
+                return (
+                  <div key={index}>
+                    <ItemLine
+                      item={item}
+                      addToInvoiceList={props.addToInvoiceFunc}
+                      invoiceList={props.invoiceList}
+                    />
+                  </div>
+                );
               }
             })
           )}
+          <ReactPaginate
+            previousLabel={t('previous')}
+            nextLabel={t('next')}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={styles.paginationButtons}
+            previousLinkClassName={styles.previousButtons}
+            nextLinkClassName={styles.nextButtons}
+            disabledClassName={styles.paginationDisabled}
+            activeClassName={styles.paginationActive}
+          />
         </div>
       ) : (
         <div className={classes.noDataCont}>
